67e8b58b6df88e89b51bbd408157c57401f46bdd
bannholtzer jonathan <bannholtzer.j@gmail.com>
2021-05-18 15:00:27 +0200

    draw snake

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-//Variables
+/** Variables **/
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-/****Interface****/
+/** Interface **/
 
 //Fond
 ctx.fillStyle = "white";
@@ -11,3 +11,28 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 //Contour
 ctx.strokeStyle = "black";
 ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+/** Structure serpent **/
+
+//Initialisation du corp
+
+let snake = [
+  { x: 140, y: 150 },
+  { x: 130, y: 150 },
+  { x: 120, y: 150 },
+  { x: 110, y: 150 },
+];
+
+function displayCube(cube) {
+  ctx.fillStyle = "#00fe14";
+  ctx.strokeStyle = "black";
+  ctx.fillRect(cube.x, cube.y, 10, 10);
+  ctx.strokeRect(cube.x, cube.y, 10, 10);
+}
+
+function displaySnake() {
+  snake.forEach((cube) => {
+    displayCube(cube);
+  });
+}
+displaySnake();
